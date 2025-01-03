ba07643c8f5ed8b9c63e3c0f7967948945f7b4e0
Lucas Alves <74011744+lucasgpalves@users.noreply.github.com>
2025-01-03 00:26:43 -0300

    update routes structure and update readme in session endpoints

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 from src import routes
+from src.authentication import generate_jwt
 
 app = FastAPI()
 app.include_router(routes.router)
+
+@app.get("/token")
+async def get_token():
+    return {"token": generate_jwt()}
--- src/routes.py
+++ src/routes.py
@@ -12,40 +12,40 @@ from .services import (
     update_task_by_id,
     remove_task_by_id,
 )
-from .authentication import authenticate, generate_jwt
+from .authentication import authenticate
 from .models import TaskCreate, TaskUpdate, TaskResponse
 from typing import List
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/tasks",
+    tags=["tasks"],
+    dependencies=[Depends(authenticate)]
+)
 
-@router.get("/token")
-async def get_token():
-    return {"token": generate_jwt()}
-
-@router.get("/tasks", response_model=List[TaskResponse])
-async def get_all_tasks(auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.get("/", response_model=List[TaskResponse])
+async def get_all_tasks(session: Session = Depends(ConnectionDB.get_session)):
     return await list_all_tasks(session)
 
-@router.get("/tasks/{id}", response_model=TaskResponse)
-async def get_task_by_id(id: int, auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.get("/{id}", response_model=TaskResponse)
+async def get_task_by_id(id: int, session: Session = Depends(ConnectionDB.get_session)):
     return await list_task_by_id(session, id)
 
-@router.get("/tasks/state/{state}", response_model=List[TaskResponse])
-async def get_task_by_state(state: str, auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.get("/state/{state}", response_model=List[TaskResponse])
+async def get_task_by_state(state: str, session: Session = Depends(ConnectionDB.get_session)):
     return await list_tasks_by_state(session, state)
 
-@router.get("/tasks/", response_model=List[TaskResponse])
-async def get_tasks_per_page(skip: int = 0, limit: int = 10, auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.get("/page/", response_model=List[TaskResponse])
+async def get_tasks_per_page(skip: int = 0, limit: int = 10, session: Session = Depends(ConnectionDB.get_session)):
     return await list_tasks_per_page(session, skip, limit)
 
-@router.post("/tasks", response_model=TaskResponse)
-async def post_new_task(task: TaskCreate, auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.post("/", response_model=TaskResponse)
+async def post_new_task(task: TaskCreate, session: Session = Depends(ConnectionDB.get_session)):
     return await create_task(session, task)
 
-@router.put("/tasks/{id}", response_model=TaskResponse)
-async def put_task_by_id(id: int,task: TaskUpdate, auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.put("/{id}", response_model=TaskResponse)
+async def put_task_by_id(id: int, task: TaskUpdate, session: Session = Depends(ConnectionDB.get_session)):
     return await update_task_by_id(session, id, task)
 
-@router.delete("/tasks/{id}", response_model=dict)
-async def delete_task_by_id(id: int, auth: dict = Depends(authenticate), session: Session = Depends(ConnectionDB.get_session)):
+@router.delete("/{id}", response_model=dict)
+async def delete_task_by_id(id: int, session: Session = Depends(ConnectionDB.get_session)):
     return await remove_task_by_id(session, id)
